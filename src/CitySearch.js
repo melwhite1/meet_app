9b38d2b5b3f76468add34bf12161892d25e522ee
melwhite1 <melissawhite668@gmail.com>
2022-09-20 10:44:18 -0400

    alerts

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,31 +1,50 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
     query: '',
     suggestions: [],
-    showSuggestions: undefined
+    showSuggestions: undefined,
+    infoText: '',
    };
 
-  handleInputChanged = (event) => {
-    const value = event.target.value;
-    const suggestions = this.props.locations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-    });
-    this.setState({ query: value, suggestions });
-  };
+   handleInputChanged = (event) => {
+     const value = event.target.value;
+     this.setState({showSuggestions:true});
+     const suggestions = this.props.locations.filter((location) => {
+       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+     });
+     if (suggestions.length === 0) {
+       this.setState({
+         query: value,
+         infoText: 'We can not find the city you are looking for. Please try another city',
+       });
+     } else {
+       return this.setState({
+         query: value,
+         suggestions,
+         infoText:''
+       });
+     }
+   };
 
-  handleItemClicked = (suggestion) => {
-    this.setState({
-      query: suggestion,
-      showSuggestions: false
+   handleItemClicked = (suggestion) => {
+     this.setState({
+       query: suggestion,
+       suggestions: [],
+       showSuggestions: false,
+       infoText: '',
     });
-    this.props.updateEvents(suggestion);
-  };
+    this.props.updateEvents(suggestion, undefined);
+};
 
   render() {
     return (
       <div className="CitySearch">
+        <div className="infoAlert">
+          <InfoAlert text={this.state.infoText} />
+        </div>
         <input
           type="text"
           className="city"
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,42 +1,45 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
-
     state = {
-        numberOfEvents: 32
+        numberOfEvents: 32,
+        infoText: '',
     };
 
     handleInputChanged = (event) => {
-        let actValue = parseInt(event.target.value)
-        if (actValue > 0 && actValue <= 32) {
-            this.setState({ numberOfEvents: actValue });
-        } else if (actValue > 32) {
-            this.setState({ numberOfEvents: 32 });
-            actValue = 32;
-        } else {
-            this.setState({ numberOfEvents: NaN });
-            actValue = NaN;
-        }
-        this.props.updateEvents(undefined, actValue);
+      let inputValue = event.target.value;
+      if (inputValue >= 33 || inputValue <= 0) {
+        this.setState({
+          numberOfEvents: inputValue,
+          infoText: 'Please enter a number between 1 - 32.',
+        });
+      } else {
+        this.setState({
+          numberOfEvents: event.target.value,
+          infoText: ' ',
+        });
+      }
+
+      this.props.updateEvents(undefined, inputValue);
     };
 
     render() {
         return (
             <div className="numberOfEvents">
                 <label htmlFor="number-of-events">Show max: </label>
-
                 <input
-                    type="number"
-                    className="number-of-events"
-                    min="1"
-                    max="32"
-                    value={this.state.numberOfEvents}
-                    onChange={this.handleInputChanged}
-
+                  type="number"
+                  id="events-num-input"
+                  value={this.state.numberOfEvents}
+                  onChange={this.handleInputChanged}
                 />
-            </div>
-        );
-    }
-}
+                <div>
+                  <ErrorAlert text={this.state.infoText} />
+                </div>
+              </div>
+            );
+          }
+        }
 
 export default NumberOfEvents;
